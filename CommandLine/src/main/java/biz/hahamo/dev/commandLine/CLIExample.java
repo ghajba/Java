1f849449ca5cd4f66fb1c98436b761e8714ea265
GHajba <hajbag@gmail.com>
2014-09-09 13:40:35 +0200

    cli improvements

--- CommandLine/src/main/java/biz/hahamo/dev/commandLine/CLIExample.java
+++ CommandLine/src/main/java/biz/hahamo/dev/commandLine/CLIExample.java
@@ -4,8 +4,6 @@ import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
@@ -17,20 +15,13 @@ public final class CLIExample {
 
     public static void main(final String... args) {
 
-        System.out.println("CLIExample is not yet implemented :( \n");
+        System.out.println("CLIExample is not yet fully implemented :( \n");
         Options options = new Options();
 
-        Option outputPath = OptionBuilder.hasArg()
-                .withDescription("Output path where the CSV file should be saved. Mandatory.").create("outputPath");
-        outputPath.setRequired(true);
-
-        Option outputFile = OptionBuilder.hasArg().withDescription("The name of the resulting CSV file. Mandatory.")
-                .create("outputFile");
-
-        outputFile.setRequired(true);
-
-        // options.addOption(outputPath);
-        // options.addOption(outputFile);
+        // example for OptionBuilder and required arguments
+        // Option outputPath = OptionBuilder.hasArg()
+        // .withDescription("Output path where the CSV file should be saved. Mandatory.").create("outputPath");
+        // outputPath.setRequired(true);
 
         options.addOption("t", "timeout", true, "Connection timeout in seconds. Optional.");
         options.addOption("h", "host", true, "Host of the proxy server. Optional. Example: 'http://localhost'");
@@ -38,17 +29,38 @@ public final class CLIExample {
 
         CommandLineParser parser = new BasicParser();
         try {
+            String errorText = "";
             CommandLine cmd = parser.parse(options, args);
-            System.out.println(cmd.getArgList());
+            if (cmd.getArgList().size() < 1) {
+                errorText = "ERROR: Parameter 'output path' is required.";
+            }
+            if (cmd.getArgList().size() < 2) {
+                errorText = "ERROR: Parameter 'output filename' is required.";
+            }
+            if (errorText.length() != 0) {
+                throw new ParseException(errorText);
+            }
 
         } catch (ParseException e) {
             System.err.println(e.getMessage());
-            HelpFormatter helpFormatter = new HelpFormatter();
-            helpFormatter
-                    .printHelp(
-                            "java -jar commandLine.jar <output path> <output filename> [-t|--timeout connection timeout] [-h|--host proxy host] [-p|--port proxy port]",
-                            options);
+            printHelp(options);
+            System.exit(-1);
         }
 
+    }
+
+    /**
+     * @param options
+     */
+    private static void printHelp(final Options options) {
+
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter
+                .printHelp(
+                        "java -jar commandLine.jar <output path> <output filename> "
+                                + "[(-t|--timeout) <connection timeout>] [(-h|--host) <proxy host>] "
+                                + "[(-p|--port) <proxy port>] "
+                                + "[other parameters1 other parameters2 ... other parametersN]",
+                        options);
     }
 }
